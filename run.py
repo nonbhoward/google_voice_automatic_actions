fb79606bcb0084f5aac8331ef933a643b731ae53
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-12-28 03:02:11 -0500

    doc bug in py wrapper to voice

--- run.py
+++ run.py
@@ -8,5 +8,6 @@ login_details = get_voice_login_details()
 email = login_details.get('email')
 passwd = login_details.get('passwd')
 # auth
+# TODO login blocked by https://github.com/jaraco/googlevoice/issues/8
 voice.login(email=email, passwd=passwd)
 # perform primary run sequence
